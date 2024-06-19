a84a9a3fc522d9a2b84613625338102a6099d85a
Phran6ix <francisolatinwo@gmail.com>
2024-06-19 14:24:22 +0100

    worker threads

--- worker.ts
+++ worker.ts
@@ -1,7 +1,7 @@
 const {parentPort } = require("worker_threads")
 
-// parentPort.once("message", (message)=> {
-// 	console.log("message")
-// 	console.log(message)
-// 	parentPort?.postMessage("Go back to parent")
-// })
+parentPort.once("message", (message)=> {
+	console.log("message")
+	console.log(message)
+	parentPort?.postMessage("Go back to parent")
+})
